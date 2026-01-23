a6f7b4547a15a0b2a70ed67358048086e5a348e9
marielav1043 <139282125+marivalerio104@users.noreply.github.com>
2026-01-23 11:11:13 -0600

    Add initial components and styles
    Add initial Header, Footer, and Note components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import '../styles/Footer.css'
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>Copyright © {new Date().getFullYear()}</p>
+    </footer>
+  )
+}
---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import '../styles/Note.css'
+
+export default function Note() {
+  return (
+    <div className='note'>
+      <h2>Note title</h2>
+      <p>Note content</p>
+    </div>
+  )
+}
